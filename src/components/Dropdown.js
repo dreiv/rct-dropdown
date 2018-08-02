4071c594d2f03ba568ea39fd6d1621b160bab7f8
dreiv <dreivoicu@gmail.com>
2018-08-02 22:31:30 +0300

    added click handler to dropdown button

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -3,15 +3,29 @@ import data from '../data.json'
 import './Dropdown.css'
 
 export default class Dropdown extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = { toggled: false }
+	}
+
+	handleToggle = () => {
+		this.setState(prevState => ({ toggled: !prevState.toggled }))
+	}
+
 	handleClick = evt => {
 		console.log('clicked')
 	}
 
 	render() {
+		const { toggled } = this.state
+		const btnText = toggled ? '-' : '+'
+		const ulStyle = !toggled ? { display: 'none' } : null
+
 		return (
 			<React.Fragment>
-				<button>+</button>
-				<ul className="items">
+				<button onClick={this.handleToggle}>{btnText}</button>
+				<ul className="items" style={ulStyle}>
 					{data.map(({ name }, idx) => (
 						<li key={idx} onClick={this.handleClick}>
 							{name}
